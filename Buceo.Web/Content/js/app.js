4a1af3d46d723d69e48943efbe43ba6d2d796eca
pzapata <pzapata@sigcomt.com>
2016-11-15 17:40:02 -0500

    avance

--- Buceo.Web/Content/js/app.js
+++ Buceo.Web/Content/js/app.js
@@ -188,80 +188,83 @@
         return false;
     });
 
-    /*
-Sand mail
-**********************************************************************/
-$("#send-mail").click(function () {
 
-        var name = $('input#name').val(); // get the value of the input field
-        var error = false;
-        if (name == "" || name == " ") {
-            $('#err-name').show(500);
-            $('#err-name').delay(4000);
-            $('#err-name').animate({
-                height: 'toggle'
-            }, 500, function () {
-                // Animation complete.
-            });
-            error = true; // change the error state to true
-        }
+//$("#send-mail").click(function () {
 
-        var emailCompare = /^([a-z0-9_.-]+)@([da-z.-]+).([a-z.]{2,6})$/; // Syntax to compare against input
-        var email = $('input#email').val().toLowerCase(); // get the value of the input field
-        if (email == "" || email == " " || !emailCompare.test(email)) {
-            $('#err-email').show(500);
-            $('#err-email').delay(4000);
-            $('#err-email').animate({
-                height: 'toggle'
-            }, 500, function () {
-                // Animation complete.
-            });
-            error = true; // change the error state to true
-        }
+//        var name = $('input#name').val(); // get the value of the input field
+//        var error = false;
+//        if (name == "" || name == " ") {
+//            $('#err-name').show(500);
+//            $('#err-name').delay(4000);
+//            $('#err-name').animate({
+//                height: 'toggle'
+//            }, 500, function () {
+//                // Animation complete.
+//            });
+//            error = true; // change the error state to true
+//        }
+
+//        var emailCompare = /^([a-z0-9_.-]+)@([da-z.-]+).([a-z.]{2,6})$/; // Syntax to compare against input
+//        var email = $('input#email').val().toLowerCase(); // get the value of the input field
+//        if (email == "" || email == " " || !emailCompare.test(email)) {
+//            $('#err-email').show(500);
+//            $('#err-email').delay(4000);
+//            $('#err-email').animate({
+//                height: 'toggle'
+//            }, 500, function () {
+//                // Animation complete.
+//            });
+//            error = true; // change the error state to true
+//        }
 
 
-        var comment = $('textarea#comment').val(); // get the value of the input field
-        if (comment == "" || comment == " ") {
-            $('#err-comment').show(500);
-            $('#err-comment').delay(4000);
-            $('#err-comment').animate({
-                height: 'toggle'
-            }, 500, function () {
-                // Animation complete.
-            });
-            error = true; // change the error state to true
-        }
+//        var comment = $('textarea#comment').val(); // get the value of the input field
+//        if (comment == "" || comment == " ") {
+//            $('#err-comment').show(500);
+//            $('#err-comment').delay(4000);
+//            $('#err-comment').animate({
+//                height: 'toggle'
+//            }, 500, function () {
+//                // Animation complete.
+//            });
+//            error = true; // change the error state to true
+//        }
 
-        if (error == false) {
-            var dataString = $('#contact-form').serialize(); // Collect data from form
-            $.ajax({
-                type: "POST",
-                url: $('#contact-form').attr('action'),
-                data: dataString,
-                timeout: 6000,
-                error: function (request, error) {
+//        if (error == false) {
+//            var dataString = $('#contact-form').serialize(); // Collect data from form
+//            $.ajax({
+//                type: "POST",
+//                url: $('#contact-form').attr('action'),
+//                data: dataString,
+//                timeout: 6000,
+//                error: function (request, error) {
 
-                },
-                success: function (response) {
-                    response = $.parseJSON(response);
-                    if (response.success) {
-                        $('#successSend').show();
-                        $("#name").val('');
-                        $("#email").val('');
-                        $("#comment").val('');
-                    } else {
-                        $('#errorSend').show();
-                    }
-                }
-            });
-            return false;
-        }
+//                },
+//                success: function (response) {
+//                    response = $.parseJSON(response);
+//                    if (response.success) {
+//                        $('#successSend').show();
+//                        $("#name").val('');
+//                        $("#email").val('');
+//                        $("#comment").val('');
+//                    } else {
+//                        $('#errorSend').show();
+//                    }
+//                }
+//            });
+//            return false;
+//        }
 
-        return false; // stops user browser being directed to the php file
-    });
+//        return false; // stops user browser being directed to the php file
+//});
 
-
-
+     $("#btnEnviarEmail").on("click", function() {
+         if (!$("#frmEnviarEmail").valid()) {
+             e.preventDefault();
+             return;
+         }
+     });
+     
     //Function for show or hide portfolio desctiption.
     $.fn.showHide = function (options) {
         var defaults = {
--- Buceo.Web/Models/MensajeEmailModel.cs
+++ Buceo.Web/Models/MensajeEmailModel.cs
@@ -1,9 +1,17 @@
-﻿namespace Buceo.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Buceo.Web.Models
 {
     public class MensajeEmailModel
     {
+        [Required(ErrorMessage = "Nombre es obligatorio")]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Email es obligatorio")]
+        [EmailAddress(ErrorMessage = "No es un correo válido")]
         public string Email{ get; set; }
+
+        [Required(ErrorMessage = "Mensaje es obligatorio")]
         public string Mensaje { get; set; }
     }
 }
